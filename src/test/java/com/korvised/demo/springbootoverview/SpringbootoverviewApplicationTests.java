20e53afbfa921f1fb9f9d9809b38d761f867ef26
Korvised YOTSAIY <kvsyotsaiy@gmail.com>
2024-11-27 13:34:35 +0700

    initial

---
+++ src/test/java/com/korvised/demo/springbootoverview/SpringbootoverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korvised.demo.springbootoverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootoverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
